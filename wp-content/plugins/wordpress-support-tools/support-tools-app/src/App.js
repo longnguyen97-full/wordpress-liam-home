319ef13635b7ed47d756031a8e34fdbc138964a2
root <root@MSI>
2022-11-09 21:38:48 +0700

    fix bugs

--- wp-content/plugins/wordpress-support-tools/support-tools-app/src/App.js
+++ wp-content/plugins/wordpress-support-tools/support-tools-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Container, Button, Grid, Box } from '@mui/material';
 import TabPanel from './components/TabPanel.jsx'
